535ab493e7134bc927c05604edb6a42e4da25fd4
Gabriela <101780994+emptyGabriela@users.noreply.github.com>
2025-09-16 11:10:52 -0600

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}","./components/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: { colors: { brand: { 500:"#6366f1", 600:"#4f46e5", 700:"#4338ca" } } }
+    extend: { colors: {
+        brand: {
+          50: "#ffffff",
+          100: "#ffffff",
+          200: "#ffffff",
+          300: "#e5eaf2",
+          400: "#d1dbe8",
+          500: "#b5c5da",
+          600: "#9ab0cc",
+          700: "#7f9bbf",
+          800: "#6485b1",
+          900: "#4f719e"
+        }
+      } } }
   },
   plugins: [],
 };
