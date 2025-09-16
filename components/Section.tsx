d169fd3a1d2bd595b0e164ca53dc92690c5d3e3d
Gabriela <101780994+emptyGabriela@users.noreply.github.com>
2025-09-16 11:30:42 -0600

    commit

--- components/Section.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-export default function Section({ id, title, subtitle, children }:{ id?: string, title?: string, subtitle?: string, children: ReactNode }){
-  return (
-    <section id={id} className="section">
-      {title && (<div className="mb-8"><h2 className="h2">{title}</h2>{subtitle && <p className="p mt-2">{subtitle}</p>}</div>)}
-      {children}
-    </section>
-  )
-}
--- components/SkillTag.tsx
+++
@@ -1,3 +0,0 @@
-export default function SkillTag({ label }:{ label:string }){
-  return <span className="badge">{label}</span>;
-}
